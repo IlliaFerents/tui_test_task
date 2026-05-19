6979681fde0730d209d83ee1fe2aaa6a70b18e20
Illia Ferents <illiaferents@gmail.com>
2026-05-19 11:48:33 +0300

    chore: configure project environment, formatting, and build tools

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig, devices } from "@playwright/test";
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+    timeout: 100000,
     testDir: "./tests",
     /* Run tests in files in parallel */
     fullyParallel: true,
@@ -25,9 +21,8 @@ export default defineConfig({
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Base URL to use in actions like `await page.goto('')`. */
-        // baseURL: 'http://localhost:3000',
-
+        baseURL: process.env.BASE_URL || "https://www.tui.nl",
+        testIdAttribute: "data-test-id",
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry"
     },
